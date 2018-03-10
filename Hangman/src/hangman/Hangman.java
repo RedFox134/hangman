b81724cf6a6c7935ad7db677d179c95beb0802b7
alexp <alexp@ALEX-PC>
2018-03-10 00:47:34 -0600

    Added a test word, menu screen of sorts, started on getting user input, and also added a way to clear the screen in command prompt.  Not the best way to get this done, but it works for now.

--- Hangman/src/hangman/Hangman.java
+++ Hangman/src/hangman/Hangman.java
@@ -5,6 +5,9 @@
  */
 package hangman;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author alexp
@@ -14,7 +17,7 @@ public class Hangman {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException, InterruptedException 
     {
         //Load Game
         String noose[][] = { 
@@ -23,17 +26,44 @@ public class Hangman {
             {"|"," "," "," ","|"," ","\n"} ,
             {"|"," "," ","/"," ","\\","\n"} ,
             {"|"," "," "," "," "," ","\n"} ,
-            {"="," "," "," "," "," ","\n"} ,
-            {"","","","","","",""}//Makes the array length long enough to print out the new line
+            {"="," "," ","","","",""} ,
+            {"","","","","","",""}//Makes the array length long enough to print out the new line character
         };
+        //Display Title
+        System.out.println("Hang Man v0.0.1");
         //Display noose
         for( int i = 0; i < noose.length; i++ )
             for( int j = 0; j < noose.length; j++ )
-            {
                 System.out.print(noose[i][j]);
-            }
         //pick random word
-        //diaply blank spaces "_" for each letter
+        String wordToGuess = "test";
+        //Need to impletment the above to actually pull from a list of words, but for now can be a constant to get the game working
+        //display blank spaces "_" for each letter
+        String blankSpaces = "_";
+        for ( int i = 0; i < wordToGuess.length(); i++ )
+            System.out.print( blankSpaces + " " );
+        //Game Menu
+        System.out.println( "Type (Y) to start a game or (N) to exit." );
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        boolean startGame = false;
+        if ( input.equalsIgnoreCase("Y") )
+            startGame = true;
+        else if ( input.equalsIgnoreCase("N") )
+            startGame = false;
+        else
+        {
+            System.out.println(input.toString());
+            System.out.print("I'm sorry, but we did not understand your request"
+                    + ".  Please try again.");
+            //recursive call to start method again?
+        }
+        //clears screen in a command propmt window
+        //does not appear to work int the netbeans console though >:(
+        //this only works on windows and chould probably be put in a try catch
+        //Source: https://stackoverflow.com/questions/2979383/java-clear-the-console
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        System.out.println("screen cleared");
         //take user input, input can be single character or word
         //check if letter or word
         //if letter check if it matches any
