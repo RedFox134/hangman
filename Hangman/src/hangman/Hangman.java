dddf105b3931fb5b1cd702c423f287d9c05d4626
E064958 <E064958@PF0UQ9AW.mcgladrey.rsm.net>
2018-03-15 16:42:00 -0500

    Started working on the running game. Added user input for guessing letters/word and started game logic

--- Hangman/src/hangman/Hangman.java
+++ Hangman/src/hangman/Hangman.java
@@ -22,9 +22,9 @@ public class Hangman {
         //Load Game
         String noose[][] = { 
             {"-","-","-","-","|"," ","\n"} ,
-            {"|"," "," ","\\","O","/","\n"} ,
-            {"|"," "," "," ","|"," ","\n"} ,
-            {"|"," "," ","/"," ","\\","\n"} ,
+            {"|"," "," "," "," "," ","\n"} ,
+            {"|"," "," "," "," "," ","\n"} ,
+            {"|"," "," "," "," "," ","\n"} ,
             {"|"," "," "," "," "," ","\n"} ,
             {"="," "," ","","","",""} ,
             {"","","","","","",""}//Makes the array length long enough to print out the new line character
@@ -40,8 +40,10 @@ public class Hangman {
         //Need to impletment the above to actually pull from a list of words, but for now can be a constant to get the game working
         //display blank spaces "_" for each letter
         String blankSpaces = "_";
+        String hiddenWord = "";
         for ( int i = 0; i < wordToGuess.length(); i++ )
-            System.out.print( blankSpaces + " " );
+            hiddenWord += blankSpaces;
+        System.out.println( hiddenWord );
         //Game Menu
         System.out.println( "Type (Y) to start a game or (N) to exit." );
         Scanner scanner = new Scanner(System.in);
@@ -53,29 +55,59 @@ public class Hangman {
             startGame = false;
         else
         {
-            System.out.println(input.toString());
             System.out.print("I'm sorry, but we did not understand your request"
                     + ".  Please try again.");
             //recursive call to start method again?
+        }
+        
+        boolean letter = false;
+        boolean word = false;
+        while( startGame )
+        {
+            //take user input, input can be single character or word
+            System.out.println("Please enter a letter or the word you would "
+                    + "like to guess: ");
+            input = scanner.next();
+            //check if letter or word
+            if ( input.length() > 1 )
+            {
+                letter = false;
+                word = true;
+            }
+            else if ( input.length() == 1)
+            {
+                letter = true;
+                word = false;
+            }
+            //if letter check if it matches any
+            if ( letter )
+                for ( int i = 0; i < wordToGuess.length(); i++ )
+                {
+                    System.out.println("i: "+i);
+                    System.out.println("wordToGuess: "+wordToGuess.charAt(i));
+                    System.out.println("input: "+input.charAt(0));
+                    System.out.println("hiddenWord: "+hiddenWord);
+                    if ( wordToGuess.charAt( i ) == input.charAt( 0 ) )
+                    {
+                        hiddenWord = hiddenWord.replace(hiddenWord.charAt( i ), input.charAt( 0 ) );
+                    }
+                }
+            System.out.println( hiddenWord );
+            //each time noose is updated clear screen and draw new noose
+            //if it does replace "_" with letter
+            //if it doesn't add body part to noose
+            //if word check if word matches
+            //if it does they win
+            //if it doesn't add two body parts
+            //check to see if player is hung
+            //if hung display word and game over
+            //if not prompt for new input
         }
         //clears screen in a command propmt window
         //does not appear to work int the netbeans console though >:(
         //this only works on windows and chould probably be put in a try catch
         //Source: https://stackoverflow.com/questions/2979383/java-clear-the-console
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        System.out.println("screen cleared");
-        //take user input, input can be single character or word
-        //check if letter or word
-        //if letter check if it matches any
-        //each time noose is updated clear screen and draw new noose
-        //if it does replace "_" with letter
-        //if it doesn't add body part to noose
-        //if word check if word matches
-        //if it does they win
-        //if it doesn't add two body parts
-        //check to see if player is hung
-        //if hung display word and game over
-        //if not prompt for new input
     }
     
 }
