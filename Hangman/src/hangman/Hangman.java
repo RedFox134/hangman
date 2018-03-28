6caf1236b9c664f41e975df2d839fde08ba14910
E064958 <E064958@PF0UQ9AW.mcgladrey.rsm.net>
2018-03-28 16:20:59 -0500

    Removed GameMenu class and updated main class to reflect this change.

--- Hangman/src/hangman/Hangman.java
+++ Hangman/src/hangman/Hangman.java
@@ -6,7 +6,6 @@
 package hangman;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 /**
  *
@@ -24,101 +23,49 @@ public class Hangman {
     {
         //Load Game
         Noose noose = new Noose();
-        //Display Title
         System.out.println("Hang Man v0.0.1");
-        //Display noose
         noose.printNoose();
-        //pick random word
-        String wordToGuess = "test";
-        //Need to impletment the above to actually pull from a list of words, but for now can be a constant to get the game working
-        //display blank spaces "_" for each letter
-        String blankSpaces = "_";
-        String hiddenWord = "";
-        for ( int i = 0; i < wordToGuess.length(); i++ )
-            hiddenWord += blankSpaces;
-        System.out.println( hiddenWord );
+        WordToGuess wordToGuess = new WordToGuess();
+        wordToGuess.printHiddenWordToGuess();
         //Game Menu
-        System.out.println( "Type (Y) to start a game or (N) to exit." );
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        boolean startGame = false;
-        if ( input.equalsIgnoreCase("Y") )
-            startGame = true;
-        else if ( input.equalsIgnoreCase("N") )
-            startGame = false;
-        else
+        System.out.println(" Type (Y) to start a game or (N) to exit.");
+        boolean incorrectGuess, startGame;
+        startGame = false;
+        Input input = new Input();
+        while( !startGame )
         {
-            System.out.print("I'm sorry, but we did not understand your request"
-                    + ".  Please try again.");
-            //recursive call to start method again?
+            input.setInput();
+            if ( input.getInput().equalsIgnoreCase( "Y" ) )
+            startGame = true;
+            else if ( input.getInput().equalsIgnoreCase( "N" ) )
+            {
+                startGame = false;
+                break;
+            }
+            else
+            {
+                System.out.println("I'm sorry,  but we did not understand your"
+                    + " request.  Please enter either (Y) for yes or (N) for no"
+                    + "");
+                System.out.println(" Type (Y) to start a game or (N) to exit.");
+            }
         }
-        
-        boolean letter = false;
-        boolean word = false;
-        boolean incorrectGuess = false;
-        while( hiddenWord.contains("_") && startGame && !noose.isHung() )
+        while( wordToGuess.getHiddenWordToGuess().contains("_") 
+                && startGame && !noose.isHung() )
         {
             //take user input, input can be single character or word
-            System.out.println("Please enter a letter or the word you would "
-                    + "like to guess: ");
-            input = scanner.next();
+            System.out.print("Please enter a letter or the word you would like"
+                    + " to guess: ");
+            input.setInput();
             //check if letter or word
-            if ( input.length() > 1 )
-            {
-                letter = false;
-                word = true;
-            }
-            else if ( input.length() == 1)
-            {
-                letter = true;
-                word = false;
-            }
             //if letter check if it matches any
-            if ( letter )
-                for ( int i = 0; i < wordToGuess.length(); i++ )
-                {
-                    if ( wordToGuess.charAt( i ) == input.charAt( 0 ) )
-                    {
-                        //if it does match replace _ with the letter
-                        int j = i;
-                        while ( j < wordToGuess.length() )
-                        {
-                            if ( wordToGuess.charAt( j ) == input.charAt( 0 ) )
-                                hiddenWord = hiddenWord.substring(0, j) + input 
-                                    + hiddenWord.substring(j+1, 
-                                            hiddenWord.length());
-                            j++;
-                        }
-                        
-                        incorrectGuess = false;
-                        letter = false;
-                        break;
-                    }
-                    //if it doesn't, add body part
-                    else
-                    {
-                        incorrectGuess = true;
-                        letter = false;
-                    }
-                }
-            else if ( word )
-                //if word matches
-                if ( wordToGuess.compareToIgnoreCase( input ) == 0 )
-                {
-                    //if it does they win
-                    hiddenWord = input;
-                    incorrectGuess = false;
-                    word = false;
-                    break;
-                } 
-                //if it doesn't add a body part
-                else
-                {
-                    incorrectGuess = true;
-                    word = false;
-                }
+            //if it does match replace _ with the letter
+            //if it doesn't, add body part
+            //if word matches
+            //if it doesn't add a body part
+            incorrectGuess = wordToGuess.checkGuess( input.getInput() );
             //if it doesn't add body part to noose
-            if ( incorrectGuess )
+            if ( !incorrectGuess )
             {
                 noose.incrementIncorrectGuessCounter();
                 noose.updateNoose();
@@ -130,49 +77,48 @@ public class Hangman {
             //Source: https://stackoverflow.com/questions/2979383/java-clear-the-console
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             noose.printNoose();
-            System.out.println( hiddenWord );
+            wordToGuess.printHiddenWordToGuess();
             //if hung display word and game over
-            if ( hiddenWord.contains( "_" ) && noose.isHung() )
+            if ( wordToGuess.getHiddenWordToGuess().contains( "_" ) 
+                    && noose.isHung() )
             {
                 System.out.println( "You were hung!" );
-                System.out.println( "The hidden word was: " + wordToGuess );
+                System.out.println( "The hidden word was: " 
+                        + wordToGuess.getWordToGuess() );
                 System.out.println( "Better luck next time!" );
             }
-            else if ( !hiddenWord.contains( "_" ) )
+            else if ( !wordToGuess.getHiddenWordToGuess().contains( "_" ) )
                 System.out.println("Congratulations!  You Win!");
             //check to restart game or not
-            if ( noose.isHung() || !hiddenWord.contains( "_" ));
+            while ( noose.isHung() || 
+                    !wordToGuess.getHiddenWordToGuess().contains( "_" ) )
             {
-                while ( noose.isHung() )
+                System.out.print( "Start a new game? (Y) or (N): " );
+                input.setInput();
+                //if not prompt for new input
+                if ( input.getInput().compareToIgnoreCase( "y" ) == 0 )
                 {
-                    System.out.print( "Start a new game? (Y) or (N): " );
-                    input = scanner.next();
-                    //if not prompt for new input
-                    if ( input.compareToIgnoreCase( "y" ) == 0 )
-                    {
-                        noose = new Noose();
-                        startGame = true;
-                        hiddenWord = "";
-                        for ( int i = 0; i < wordToGuess.length(); i++ )
-                            hiddenWord += blankSpaces;
-                    }
-                    else if ( input.compareToIgnoreCase( "n" ) == 0 )
-                    {
-                        noose = new Noose();
-                        startGame = false;
-                    }
-                    else
-                        System.out.println("I'm sorry, but we did not "
-                                + "recoginize that input.  Please enter either "
-                                + "Y for yes or N for no.");
+                    noose = new Noose();
+                    startGame = true;
+                    wordToGuess = new WordToGuess();
                 }
+                else if ( input.getInput().compareToIgnoreCase( "n" ) == 0 )
+                {
+                    noose = new Noose();
+                    startGame = false;
+                    break;
+                }
+                else
+                    System.out.println("I'm sorry, but we did not "
+                            + "recoginize that input.  Please enter either "
+                            + "Y for yes or N for no.");
             }
         }
         //clears screen in a command prompt window
         //does not appear to work in the netbeans console though >:(
         //this only works on windows and should probably be put in a try catch
         //Source: https://stackoverflow.com/questions/2979383/java-clear-the-console
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        //new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
     
 }
